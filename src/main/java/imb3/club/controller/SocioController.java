2c36f2072d0adea0d35bacd72c0c09184d35265b
Ivan Balmaceda <balmacedaivan5@gmail.com>
2023-06-17 20:29:32 -0300

    Creacion de SocioController

--- src/main/java/imb3/club/controller/SocioController.java
+++ src/main/java/imb3/club/controller/SocioController.java
@@ -1,2 +1,22 @@
 package imb3.club.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import imb3.club.entity.Socio;
+import imb3.club.service.ISocioService;
+
+@RestController
+@RequestMapping("/api/v1")
+public class SocioController {
+	
+	@Autowired
+	private ISocioService service;
+	
+	public List<Socio> getSocios() {
+		return service.mostrarSocios();
+	}
+	
+}
