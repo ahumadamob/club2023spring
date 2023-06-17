b0ea8f7437739155bf451f6b593fbcfca06662cf
Ivan Balmaceda <balmacedaivan5@gmail.com>
2023-06-17 20:26:57 -0300

    Creacion de SocioService

--- src/main/java/imb3/club/service/jpa/SocioService.java
+++ src/main/java/imb3/club/service/jpa/SocioService.java
@@ -1,3 +1,23 @@
 package imb3.club.service.jpa;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import imb3.club.entity.Socio;
+import imb3.club.repository.SocioRepository;
+import imb3.club.service.ISocioService;
+
+@Service
+public class SocioService implements ISocioService {
+	
+	@Autowired
+	private SocioRepository repo;
+
+	@Override
+	public List<Socio> mostrarSocios() {
+		return repo.findAll();
+	}
+
+}
