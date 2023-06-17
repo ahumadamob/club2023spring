dca775a9fb361e09417d93ef6d70a2da14418294
Ivan Balmaceda <balmacedaivan5@gmail.com>
2023-06-17 20:24:22 -0300

    Creacion de SocioRepository

--- src/main/java/imb3/club/repository/SocioRepository.java
+++ src/main/java/imb3/club/repository/SocioRepository.java
@@ -1,2 +1,9 @@
 package imb3.club.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import imb3.club.entity.Socio;
+
+public interface SocioRepository extends JpaRepository<Socio, Integer> {
+	
+}
