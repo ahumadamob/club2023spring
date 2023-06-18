5760d3900313a8361b7a0d6d918b79a403cc4f40
Ivan Balmaceda <balmacedaivan5@gmail.com>
2023-06-18 19:24:20 -0300

    Configuracion de application.properties

--- src/main/java/imb3/club/controller/SocioController.java
+++ src/main/java/imb3/club/controller/SocioController.java
@@ -3,6 +3,7 @@ package imb3.club.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import imb3.club.entity.Socio;
