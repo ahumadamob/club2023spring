5d02ccbe66cecd0d9a67864d6e34069998083984
LaureanoChirino <nano_chirino@hotmail.com>
2023-06-19 14:15:32 -0300

    Agregando ExceptionHandler al controller

--- src/main/java/imb3/club/controllers/DisciplinaController.java
+++ src/main/java/imb3/club/controllers/DisciplinaController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +18,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import imb3.club.entity.Disciplina;
 import imb3.club.service.IDisciplinaService;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 
 @RestController
 @RequestMapping("/api/v1/disciplinas")
@@ -128,8 +131,18 @@ public class DisciplinaController {
 			}
 		}
 	}
+
+@ExceptionHandler(ConstraintViolationException.class)
+public ResponseEntity<APIResponse<?>> handleConstraintViolationException(ConstraintViolationException ex){
+	List<String> errors = new ArrayList<>();
+	for (ConstraintViolation<?> violation: ex.getConstraintViolations()) {
+		
+		errors.add(violation.getMessage());
+	}
+	APIResponse<Disciplina> response = new APIResponse<Disciplina>(HttpStatus.BAD_REQUEST.value(), errors, null);
+	return ResponseEntity.badRequest().body(response);
 	
 	
-	
+}
 	
 }
