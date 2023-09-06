ffe69d85056cfad34c6d28cba8cdc93103d671f9
Ivan Balmaceda <balmacedaivan5@gmail.com>
2023-09-06 16:00:53 -0300

    Pruebas issues rama 7

--- src/main/java/imb/pr2/club/entity/Clase.java
+++ src/main/java/imb/pr2/club/entity/Clase.java
@@ -1,8 +1,12 @@
 package imb.pr2.club.entity;
+import java.sql.Date;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -14,12 +18,23 @@ public class Clase {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	
-	@NotNull(message = "El id del socio no puede estar vacío") 
-	private Integer id_Socio;
+	@ManyToOne
+	@JoinColumn(name="socioId")
+	private Socio socio;
 	
-	@NotNull(message = "El id del profesor no puede estar vacío")
-	private Integer id_Profesor;
+	@ManyToOne
+	@JoinColumn(name="profeId")
+	private Profesor profe;
 	
+	/*@NotNull(message = "El id del profesor no puede estar vacío")
+	private Integer id_Profesor;*/
+	
+	public Profesor getProfe() {
+		return profe;
+	}
+	public void setProfe(Profesor profe) {
+		this.profe = profe;
+	}
 	@NotNull(message = "El id de disciplina no puede estar vacío")
 	private Integer Id_Disciplina;
 	
@@ -33,24 +48,40 @@ public class Clase {
 	@NotNull(message = "El horario no puede estar vacío")
 	private Integer horario;
 	
+	
+	/*@NotNull(message = "Fecha y hora")
+	private Date date;*/
+	
+	
+	
+	
+	
 	public Integer getId() {
 		return id;
 	}
 	public void setId(Integer id) {
 		this.id = id;
 	}
+	
+	/*
 	public Integer getId_Socio() {
 		return id_Socio;
 	}
 	public void setId_Socio(Integer id_Socio) {
 		this.id_Socio = id_Socio;
 	}
+	*/
+	
+	
+	/*
 	public Integer getId_Profesor() {
 		return id_Profesor;
 	}
 	public void setId_Profesor(Integer id_Profesor) {
 		this.id_Profesor = id_Profesor;
 	}
+	*/
+	
 	public Integer getId_Disciplina() {
 		return Id_Disciplina;
 	}
@@ -75,5 +106,25 @@ public class Clase {
 	public void setHorario(Integer horario) {
 		this.horario = horario;
 	}
+	
+	
+	/*public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}*/
+	
+	
+	public Socio getSocio() {
+		return socio;
+	}
+	public void setSocio(Socio socio) {
+		this.socio = socio;
+	}
+	
+	
+	
+	
 
 }
