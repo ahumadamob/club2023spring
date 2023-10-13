007d86ac468f6b96d0545319abdfe7a9db927d40
LaureanoChirino <nano_chirino@hotmail.com>
2023-10-13 13:21:59 -0300

    Se agrego clase ResponseUtil y mejoras en SocioController

--- src/main/java/imb/pr2/club/controller/SocioController.java
+++ src/main/java/imb/pr2/club/controller/SocioController.java
@@ -1,9 +1,7 @@
 package imb.pr2.club.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -17,7 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import imb.pr2.club.entity.Socio;
 import imb.pr2.club.service.ISocioService;
-import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
 
 
@@ -26,92 +23,77 @@ import jakarta.validation.ConstraintViolationException;
 public class SocioController {
 	
 	@Autowired
-	private ISocioService service;
+	private ISocioService socioService;
 	
 	
 	@GetMapping
 	public ResponseEntity<APIResponse<List<Socio>>> buscarTodosLosSocios() {
-		List<String> messages = new ArrayList<>();
-		messages.add("Socios registrados");
-		APIResponse<List<Socio>> response = new APIResponse<List<Socio>>(200, messages, service.buscarTodos());
-		return ResponseEntity.status(HttpStatus.OK).body(response);	
+		List<Socio> socios = socioService.buscarTodos();
+		return (socios.isEmpty()) ? ResponseUtil.notFound("No se encontraron Socios.") 
+				: ResponseUtil.success(socios);
 	}
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<APIResponse<Socio>> buscarSocioPorId(@PathVariable("id") Integer id) {
-		if(service.existe(id)) {
-			Socio socio = service.buscarPorId(id);
-			APIResponse<Socio> response = new APIResponse<Socio>(HttpStatus.OK.value(), null, socio);
-			return ResponseEntity.status(HttpStatus.OK).body(response);	
-		}else {
-			List<String> messages = new ArrayList<>();
-			messages.add("No se encontró la Categoría con id = " + id.toString());
-			messages.add("Revise nuevamente el parámetro");
-			APIResponse<Socio> response = new APIResponse<Socio>(HttpStatus.BAD_REQUEST.value(), messages, null);
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-			}
+		
+		return (socioService.existe(id)) ? ResponseUtil.success(socioService.buscarPorId(id)) 
+				: ResponseUtil.notFound("No se encontro un Socio con el Id especificado.");
+		
 		}
 	
 	@PostMapping
 	public ResponseEntity<APIResponse<Socio>> crearSocio(@RequestBody Socio socio) {
-		if(service.existe(socio.getId())) {
-			List<String> messages = new ArrayList<>();
-			messages.add("Ya existe una categoria con el ID = " + socio.getId().toString());
-			messages.add("Para actualizar utilice el verbo PUT");
-			APIResponse<Socio> response = new APIResponse<Socio>(HttpStatus.BAD_REQUEST.value(), messages, null);
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-		}else {
-			service.guardar(socio);
-			List<String> messages = new ArrayList<>();
-			messages.add("Socio registrado con exito");
-			APIResponse<Socio> response = new APIResponse<Socio>(HttpStatus.CREATED.value(), messages, socio);
-			return ResponseEntity.status(HttpStatus.CREATED).body(response);			
-		}			
+		
+		return (socioService.existe(socio.getId())) ? ResponseUtil.badRequest("Ya existe un Socio con este Id.")
+				: ResponseUtil.created(socioService.guardar(socio));
+			
 	}
-	
 	
 	@PutMapping
 	public ResponseEntity<APIResponse<Socio>> modificarSocio(@RequestBody Socio socio) {
-		if(service.existe(socio.getId())) {
-			service.guardar(socio);
-			List<String> messages = new ArrayList<>();
-			messages.add("Datos de Socio modificados con exito");
-			APIResponse<Socio> response = new APIResponse<Socio>(HttpStatus.OK.value(), messages, socio);
-			return ResponseEntity.status(HttpStatus.OK).body(response);
-		}else {
-			List<String> messages = new ArrayList<>();
-			messages.add("No existe un socio con el ID especificado");
-			messages.add("Para crear uno nuevo utilice el verbo POST");
-			APIResponse<Socio> response = new APIResponse<Socio>(HttpStatus.BAD_REQUEST.value(), messages, null);
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
-		}
+		
+		return (socioService.existe(socio.getId())) ? ResponseUtil.success(socioService.guardar(socio)) 
+				: ResponseUtil.notFound("No se encontro un Socio con el Id especificado.");
 	}
+	
+	/*-----------------------------------------------------------------------------------------------------------------------------
+	 													EXPLICACIÓN MÉTODO POST
+	  -----------------------------------------------------------------------------------------------------------------------------
+	1- @PutMapping es una anotación que indica que el método Java que sigue a esta anotación manejará solicitudes HTTP de tipo POST
+	2- El método devolverá un objeto ResponseEntity que contiene un tipo genérico APIResponse<Socio>. ResponseEntity se utiliza para 
+	encapsular la respuesta HTTP que se enviará de vuelta al cliente.
+	3- crearSocio: Es el nombre que le vamos a dar al método.
+	4- @RequestBody Socio socio: Esta anotación indica que el parámetro socio se vinculará al cuerpo de la solicitud HTTP 
+	y se convertirá automáticamente en un objeto Java de tipo Socio.
+	5- socioService.existe(socio.getId()): Llama a al método existe en el servicio llamado socioService y pasa el ID del Socio 
+	como argumento. El método existe comprueba si ya existe un socio en la base de datos con el mismo ID.
+	6- Si existe devuelve true y se ejecuta la siguiente parte del código:
+		-> ResponseUtil.badRequest("Ya existe un Socio con este Id."): Esto crea una respuesta de error (código 400 - Bad Request) 
+		utilizando el método badRequest de ResponseUtil y proporciona un mensaje de error indicando que ya existe un socio con 
+		el mismo ID.
+	7- Si no existe devuelve false y se ejecuta la siguiente parte del código:
+		-> ResponseUtil.created(socioService.guardar(socio)): Esto crea una respuesta exitosa (código 201 - Created) 
+		utilizando el método created de ResponseUtil y llama al método socioService.guardar(socio) para guardar el nuevo socio
+		 en la base de datos.
+	
+	-------------------------------------------------------------------------------------------------------------------------------
+	*/
 	
 	@DeleteMapping("/{id}")	
 	public ResponseEntity<APIResponse<Socio>> eliminarSocio(@PathVariable("id") Integer id) {
-		if(service.existe(id)) {
-			service.eliminar(id);
-			List<String> messages = new ArrayList<>();
-			messages.add("El Socio fue eliminado de la Base de Datos") ;			
-			APIResponse<Socio> response = new APIResponse<Socio>(HttpStatus.OK.value(), messages, null);
-			return ResponseEntity.status(HttpStatus.OK).body(response);	
-		}else {
-			List<String> messages = new ArrayList<>();
-			messages.add("No existe un Socio con el ID = " + id.toString());
-			APIResponse<Socio> response = new APIResponse<Socio>(HttpStatus.BAD_REQUEST.value(), messages, null);
-			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);			
-		}
 		
+		if(socioService.existe(id)) {
+			socioService.eliminar(id);
+			return ResponseUtil.success(null);
+		}else {
+			return ResponseUtil.notFound("No se encontro un Socio con el Id especificado.");
+		}
 	}
 	
 	@ExceptionHandler(ConstraintViolationException.class)
-	public ResponseEntity<APIResponse<?>> handleConstraintViolationException(ConstraintViolationException ex){
-		List<String> errors = new ArrayList<>();
-		for(ConstraintViolation<?> violation : ex.getConstraintViolations()) {
-			errors.add(violation.getMessage());
-		}
-		APIResponse<Socio> response = new APIResponse<Socio>(HttpStatus.BAD_REQUEST.value(), errors, null);
-		return ResponseEntity.badRequest().body(response);
+	public ResponseEntity<APIResponse<Object>> handleConstraintViolationException(ConstraintViolationException ex){
+	
+		return ResponseUtil.handleConstraintException(ex);
 	}
 	
 	
